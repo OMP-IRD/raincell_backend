c9dfa05ef1f7c53022b1936b0c759ca8ea3e7eb9
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2022-06-08 14:44:23 +0200

    Add batch import. Improve code structure

---
+++ raincell_core/management/commands/_import_tools.py
@@ -0,0 +1,62 @@
+from django.contrib.gis.geos import Point
+from raincell_core.models import CellRecord
+from django.db import transaction
+
+import netCDF4
+import datetime
+import os
+import statistics
+
+
+@transaction.atomic
+def import_file(file_path, mask_path, verbose=False):
+    coordinates_decimals = os.environ.get('RAINCELL_COORDINATES_ROUND_DECIMALS')
+
+    nc = netCDF4.Dataset(file_path, "r", format="netCDF4")
+    nc_mask = netCDF4.Dataset(mask_path, "r", format="netCDF4")
+    # ds = xr.open_dataset(file_path)
+    # df = ds.to_dataframe()
+    counter = 0
+    for ilon in range(nc.variables['longitude'].size):
+        for ilat in range(nc.variables['latitude'].size):
+            # print("{},{}".format(ilon, ilat))
+            # print(nc_mask.variables['mask'][ilat, ilon])
+            lat = nc.variables['latitude'][ilat].item()
+            lon = nc.variables['longitude'][ilon].item()
+            if coordinates_decimals:
+                lat = round(lat, int(coordinates_decimals))
+                lon = round(lon, int(coordinates_decimals))
+            if nc_mask.variables['mask'][ilat, ilon].item() > 0:
+                if (verbose):
+                    print("Coordinates: {} (lon) / {} (lat): {},{},{}".
+                        format(
+                            lat,
+                            lon,
+                            nc.variables['Rainfall'][0, ilat, ilon].item(),
+                            nc.variables['Rainfall'][1, ilat, ilon].item(),
+                            nc.variables['Rainfall'][2, ilat, ilon].item()
+                        )
+                    )
+                filename = os.path.basename(file_path)
+                file_day = filename.split('_')[0]
+                file_day_date = datetime.datetime.strptime(file_day, "%Y%m%d").date()
+                file_time = filename.split('_')[1]
+                location = Point(lat, lon)
+                recs = CellRecord.objects.filter(location__equals=location,
+                                                 day=file_day_date,
+                                                 # day__year=file_day[0:4],
+                                                 # day__month=file_day[4:6],
+                                                 # day__day=file_day[6:8],
+                                                 )
+                rec = recs.first()
+                if rec is None:  # queryset was empty
+                    rec = CellRecord.objects.create(location=location,
+                                                    day=file_day_date,
+                                                    )
+                rec.quantile25[file_time] = nc.variables['Rainfall'][0, ilat, ilon].item()
+                rec.quantile50[file_time] = nc.variables['Rainfall'][1, ilat, ilon].item()
+                rec.quantile75[file_time] = nc.variables['Rainfall'][2, ilat, ilon].item()
+                rec.daily_mean = statistics.mean(rec.quantile50.values())
+                rec.save()
+                counter += 1
+    return counter
--- raincell_core/management/commands/raincell_import_file.py
+++ raincell_core/management/commands/raincell_import_file.py
@@ -2,15 +2,14 @@ from django.core.management.base import BaseCommand, CommandError
 
 from . import _import_tools
 
-import glob
 from os import environ
 from time import perf_counter
 
 class Command(BaseCommand):
-    help = 'Recursively batch import raincell netcdf files'
+    help = 'Import a netcdf data file into the DB (1 file/datetime at a time)'
 
     def add_arguments(self, parser):
-        parser.add_argument('folder_path')
+        parser.add_argument('file_path')
 
         # Named (optional) arguments
         parser.add_argument(
@@ -24,9 +23,9 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **kwargs):
-        global_start_time = perf_counter()
+        start_time = perf_counter()
 
-        folder_path = kwargs['folder_path']
+        file_path = kwargs['file_path']
         verbose = kwargs.get('verbose', False)
 
         # get mask path value. Best practice would be to use environ variable. But allow also as argument
@@ -34,15 +33,7 @@ class Command(BaseCommand):
         if not mask_path:
             raise CommandError("Path to mask file should be defined. Either use the env. var `RAINCELL_NETCDF_MASK_PATH` of the command option `mask_path`")
 
-        files_counter = 0
-        for file_path in glob.iglob(folder_path + '**/20*.nc', recursive=True):
-            start_time = perf_counter()
-            print(file_path)
-            files_counter += 1
-            counter = _import_tools.import_file(file_path, mask_path, verbose)
-            end_time = perf_counter()
-            self.stdout.write(self.style.SUCCESS('{} processed in {} seconds ({} records) '.format(file_path, end_time - start_time, counter)))
+        counter = _import_tools.import_file(file_path, mask_path, verbose)
 
-        global_end_time = perf_counter()
-        self.stdout.write(self.style.SUCCESS('Elapsed time: {}'.format(global_end_time - global_start_time)))
-        self.stdout.write(self.style.SUCCESS('Processed files: {}'.format(files_counter)))
+        end_time = perf_counter()
+        self.stdout.write(self.style.SUCCESS('{} processed in {} seconds ({} records) '.format(file_path, end_time - start_time, counter)))
